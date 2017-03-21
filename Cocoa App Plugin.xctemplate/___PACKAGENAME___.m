4c95a7746665e8225e03f180c5bf3c5f240a835f
corbin <hongbin.chan@qq.com>
2017-03-21 16:16:35 +0800

    Add Mac App Plugin Project Template

---
+++ Cocoa App Plugin.xctemplate/___PACKAGENAME___.m
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAME___.h"
+#import "___PACKAGENAME___Header.h"
+
+#pragma mark - Plugin
+
+@implementation NSObject (___PACKAGENAME___)
+
+@end
+
+@implementation NSView (___PACKAGENAME___)
+
+@end
+
+@implementation NSViewController (___PACKAGENAME___)
+
+@end
+
+static void __attribute__((constructor)) initialize(void) {
+    NSLog(@"++++++++ ___FILEBASENAME___ loaded ++++++++");
+}
